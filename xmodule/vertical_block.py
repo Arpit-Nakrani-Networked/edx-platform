c3b5129b629f5d5aa0de47005af4082466587468
Arpit-Nakrani-Networked <arpit.nakrani@networked.co>
2025-09-02 07:23:18 +0200

    FIX | mark-completed-on-view-after-delay 0ms

--- xmodule/vertical_block.py
+++ xmodule/vertical_block.py
@@ -102,6 +102,7 @@ class VerticalBlock(
         completion_service = self.runtime.service(self, 'completion')
         if completion_service and completion_service.completion_tracking_enabled():
             child_blocks_to_complete_on_view = completion_service.blocks_to_mark_complete_on_view(child_blocks)
+            complete_on_view_delay = 0
 
         child_context['child_of_vertical'] = True
         is_child_of_vertical = context.get('child_of_vertical', False)
@@ -114,7 +115,7 @@ class VerticalBlock(
             child_block_context = copy(child_context)
             if child in list(child_blocks_to_complete_on_view):
                 child_block_context['wrap_xblock_data'] = {
-                    'mark-completed-on-view-after-delay': 1
+                    'mark-completed-on-view-after-delay': complete_on_view_delay
                 }
             try:
                 # .. filter_implemented_name: VerticalBlockChildRenderStarted
