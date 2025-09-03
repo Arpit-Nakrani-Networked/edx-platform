e15595f6a459fd86895cf4b87d5b5e90829ec56e
Arpit-Nakrani-Networked <arpit.nakrani@networked.co>
2025-09-03 14:59:02 +0200

    FIX | course_visibility added in course list studio

--- cms/djangoapps/contentstore/rest_api/v1/serializers/course_details.py
+++ cms/djangoapps/contentstore/rest_api/v1/serializers/course_details.py
@@ -57,3 +57,5 @@ class CourseDetailsSerializer(serializers.Serializer):
     title = serializers.CharField(allow_blank=True)
     video_thumbnail_image_asset_path = serializers.CharField()
     video_thumbnail_image_name = serializers.CharField(allow_blank=True)
+    course_visibility = serializers.CharField()
+
